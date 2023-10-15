f89599bc5dd21b8e6de780c52ebfcaa2a6783680
antimatter15 <antimatter15@gmail.com>
2023-10-15 11:53:48 -0700

    basic localstorage contract

---
+++ basic-storage/contract.js
@@ -0,0 +1,22 @@
+window.addEventListener('message', function(event) {
+    const data = event.data;
+    if (data && data.action) {
+        if (data.action === 'save' && data.value !== undefined) {
+            localStorage.setItem('data', data.value);
+        } else if (data.action === 'load') {
+            postToParent(localStorage.getItem('data'));
+        }
+    }
+}, false);
+
+window.addEventListener('storage', function(event) {
+    if (event.key === 'data') {
+        postToParent(event.newValue);
+    }
+});
+
+function postToParent(value) {
+    window.parent.postMessage(value, '*'); // '*' means we're sending to any domain
+}
+
+postToParent(localStorage.getItem('data'));
