5e2a8250a3876d459629ea24d83ff457db1a8a4e
antimatter15 <antimatter15@gmail.com>
2025-05-13 00:44:22 -0700

    mcp over caddr

---
+++ mcp/api.js
@@ -0,0 +1,336 @@
+let servers = {};
+let channels = {};
+
+function showServers() {
+  dataBox.innerText = "";
+  for (let info of Object.values(servers)) {
+    const block = document.createElement("div");
+    block.innerText = `${info.from} (${info.fromOrigin})`;
+    for (let method of info.result) {
+      const button = document.createElement("button");
+      button.innerText = method;
+      button.addEventListener("click", () => {
+        RPC(info.from, method, prompt("Input for " + method)).then((result) => {
+          alert(JSON.stringify(result));
+        });
+      });
+      block.appendChild(button);
+    }
+    dataBox.appendChild(block);
+  }
+}
+
+function setupDataChannel(clientId, channel) {
+  channels[clientId] = new Promise((resolve, reject) => {
+    channel.onopen = () => {
+      console.log("Channel is open!");
+      resolve(channel);
+      // channel.send("Hello from Peer!");
+    };
+
+    channel.onmessage = (event) => {
+      console.log("Message received on:", event.data);
+      handleMessage({
+        ...JSON.parse(event.data),
+        ...senders[clientId],
+      });
+    };
+
+    channel.onclose = (event) => {
+      console.log("channel closed", event);
+      delete channels[clientId];
+    };
+  });
+}
+let senders = {};
+async function connectDataChannel(clientId) {
+  const peerA = new RTCPeerConnection({
+    //   iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+  });
+  peers[clientId] = peerA;
+  const channelA = peerA.createDataChannel("myChannel");
+  setupDataChannel(clientId, channelA);
+  peerA.onconnectionstatechange = () => {
+    console.log(`Peer A connection state: ${peerA.connectionState}`);
+  };
+  peerA.oniceconnectionstatechange = () => {
+    console.log(`Peer A ICE state: ${peerA.iceConnectionState}`);
+  };
+
+  let earlyCandidates = [];
+  peerA.onicecandidate = (event) => {
+    if (event.candidate) {
+      earlyCandidates.push(event.candidate.toJSON());
+      console.log("CANDIDATE EARLY", event);
+    }
+  };
+  const offer = await peerA.createOffer();
+  await peerA.setLocalDescription(offer);
+  console.log("got offer", offer);
+  const answer = await RPC(clientId, "connectRTC", offer);
+  console.log("got answer", answer);
+  await peerA.setRemoteDescription(answer);
+  for (let candidate of earlyCandidates) {
+    NOTIFY(clientId, "addICE", candidate);
+  }
+  peerA.onicecandidate = (event) => {
+    if (event.candidate) {
+      console.log("sending ice candidate", event);
+      NOTIFY(clientId, "addICE", event.candidate.toJSON());
+    }
+  };
+}
+
+let peers = {};
+const requestHandlers = {
+  async echo(sender, data) {
+    return "Echo: " + data;
+  },
+  async setBackgroundColor(sender, color) {
+    document.body.style.background = color;
+  },
+  async listMethods() {
+    return Object.keys(requestHandlers);
+  },
+  async connectRTC(sender, offer) {
+    console.log("got offer", offer);
+    const peerB = new RTCPeerConnection({
+      // iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+    });
+    peers[sender.from] = peerB;
+    peerB.onconnectionstatechange = () => {
+      console.log(`Peer B connection state: ${peerB.connectionState}`);
+    };
+    peerB.oniceconnectionstatechange = () => {
+      console.log(`Peer B ICE state: ${peerB.iceConnectionState}`);
+    };
+    peerB.onicecandidate = (event) => {
+      if (event.candidate) {
+        console.log("sending ice candidate", event);
+        NOTIFY(sender.from, "addICE", event.candidate.toJSON());
+      }
+    };
+    peerB.ondatachannel = (event) => {
+      const channelB = event.channel;
+      setupDataChannel(sender.from, channelB);
+    };
+
+    await peerB.setRemoteDescription(offer);
+    const answer = await peerB.createAnswer();
+    await peerB.setLocalDescription(answer);
+    console.log("got answer", answer);
+    return answer;
+  },
+  async addICE(sender, candidate) {
+    peers[sender.from].addIceCandidate(candidate);
+  },
+  async addServer(sender, methods) {
+    servers[sender.from] = {
+      from: sender.from,
+      fromOrigin: sender.fromOrigin,
+      result: methods,
+    };
+    // showServers();
+    return await requestHandlers.listMethods();
+  },
+  async markAlive(sender, clientIds) {
+    for (let key in servers) {
+      if (!clientIds.includes(key) && key !== sender.from) {
+        delete servers[key];
+      }
+    }
+    // showServers();
+  },
+  async onConnect(sender, params) {
+    if (sender.from === "") {
+      hasBroadcastChannel = params.hasBroadcastChannel;
+      // hideAuthorizationFrame();
+      // dataBox.innerText = "Listing methods...";
+
+      const allServers = await RPC(
+        "*",
+        "addServer",
+        await requestHandlers.listMethods()
+      );
+      servers = {};
+      for (let info of allServers) {
+        servers[info.from] = info;
+      }
+      await NOTIFY("*", "markAlive", Object.keys(servers));
+      // showServers();
+    }
+  },
+};
+
+async function NOTIFY(clientId, method, params) {
+  const forceTunnel = ["connectRTC", "addICE"].includes(method);
+  sendRPCMessage(
+    {
+      to: clientId,
+      method: method,
+      params: params,
+    },
+    forceTunnel
+  );
+}
+
+async function RPC(clientId, method, params) {
+  // genreally responds whenever first reply recieved, except for where clientId='*'
+  // then we wait until a timeout and return an array with all replies.
+
+  const replyId = replyIdCounter++;
+  console.log("-->", clientId, replyId, method, params);
+  return new Promise((resolve, reject) => {
+    const forceTunnel = ["connectRTC", "addICE"].includes(method);
+    sendRPCMessage(
+      {
+        to: clientId,
+        id: replyId,
+        method: method,
+        params: params,
+      },
+      forceTunnel
+    );
+    if (clientId === "*") {
+      const replies = {};
+      replyCallbacks[replyId] = (reply) => {
+        replies[reply.from] = reply;
+      };
+      setTimeout(() => {
+        delete replyCallbacks[replyId];
+        resolve(Object.values(replies));
+      }, 2000);
+    } else {
+      replyCallbacks[replyId] = (reply) => {
+        delete replyCallbacks[replyId];
+        if (reply.error) {
+          reject(reply.error);
+        } else {
+          resolve(reply.result);
+        }
+      };
+    }
+  });
+}
+
+// send(clientId, method, data)
+// how do replies work? we have multiple replies from multiple clients because broadcast is ok i guess.
+const replyCallbacks = {};
+let replyIdCounter = 1;
+
+function handleMessage(data) {
+  if (requestHandlers[data.method]) {
+    console.log("<--", data.from, data.method, data.params);
+    const sender = {
+      from: data.from,
+      fromOrigin: data.fromOrigin,
+    };
+    senders[data.from] = sender;
+    const forceTunnel = ["connectRTC", "addICE"].includes(data.method);
+    requestHandlers[data.method](sender, data.params).then(
+      (result) => {
+        if (data.id) {
+          sendRPCMessage(
+            {
+              to: data.from,
+              result: result,
+              id: data.id,
+            },
+            forceTunnel
+          );
+        }
+      },
+      (error) => {
+        console.error(error);
+        if (data.id) {
+          sendRPCMessage(
+            {
+              to: data.from,
+              error: error,
+              id: data.id,
+            },
+            forceTunnel
+          );
+        }
+      }
+    );
+  } else if (data.id) {
+    console.log("<--", data.from, data);
+    if (replyCallbacks[data.id]) {
+      replyCallbacks[data.id](data);
+    } else {
+      console.log("no reply callback for id", data.id, data);
+    }
+  } else {
+    console.log("<--", data);
+  }
+}
+
+function sendRPCMessage(message, forceTunnel = false) {
+  const payload = JSON.stringify(message);
+  if (hasBroadcastChannel) forceTunnel = true;
+  if (message.to === "*") forceTunnel = true;
+  if (channels[message.to] && !forceTunnel) {
+    console.log("sending message to channel", message);
+    channels[message.to].then((channel) => {
+      channel.send(payload);
+    });
+  } else if (!forceTunnel && payload.length > 1024) {
+    console.log("connecting to channel", message);
+    connectDataChannel(message.to);
+    channels[message.to].then((channel) => {
+      channel.send(payload);
+    });
+  } else {
+    console.log("sending message", message);
+    caddrFrame.contentWindow.postMessage(message, hostOrigin);
+  }
+}
+
+window.addEventListener(
+  "message",
+  function (event) {
+    // console.log(event.origin, event.data)
+    // Always check the origin for security reasons
+    if (event.origin === hostOrigin) {
+      if (event.data.action === "redirect") {
+        let url = new URL(event.data.value);
+        url.searchParams.set("authRedirect", location.href);
+        location.href = url;
+        return;
+      }
+      const data = JSON.parse(event.data.value);
+      handleMessage(data);
+    }
+  },
+  false
+);
+
+const contractURL = new URL("contract.js", location.href);
+contractURL.searchParams.set("date", Date.now());
+let hostOrigin = "";
+let hasBroadcastChannel = false;
+
+async function getHash32(url) {
+  let n =
+      (new Uint8Array(
+        await fetch(url)
+          .then((k) => k.arrayBuffer())
+          .then((k) => crypto.subtle.digest("SHA-256", k))
+      ).reduce((acc, k) => acc * 256n + BigInt(k), 0n) *
+        256n ** 3n) /
+      32n ** 3n,
+    res = "";
+  while ((n /= 32n) !== 0n)
+    res = "abcdefghijklmnopqrstuvwxyz234567"[Number(n % 32n)] + res;
+  return res;
+}
+
+getHash32(contractURL).then((hash32) => {
+  hostOrigin = "https://" + hash32 + ".caddr.org";
+  let url = new URL(hostOrigin);
+  url.searchParams.set("src", contractURL);
+  if (new URLSearchParams(location.search).get("useRedirect"))
+    url.searchParams.set("useRedirect", "true");
+  caddrFrame.setAttribute("src", url);
+});
---
+++ mcp/contract.js
@@ -0,0 +1,234 @@
+const button = document.createElement("button");
+const url = new URL(location.href);
+const clientId = Math.random().toString(36).substring(2, 15);
+let lastValue = "";
+
+function postToParent(value) {
+  const ancestor = window.opener || window.parent;
+  if (window === ancestor) return;
+  ancestor.postMessage({ action: "data", value: value }, "*");
+}
+
+function mountHandle(handle) {
+  const processedMessages = new Set();
+  const pendingCookies = [];
+
+  const getValues = () => {
+    const cookies = document.cookie
+      .split(";")
+      .map((k) => k.trim().split("=").map(decodeURIComponent));
+    const observedKeys = new Set();
+    for (let [key, data] of cookies) {
+      let [prefix, destination, requestId] = key.split("_");
+      if (prefix !== "rpc") continue;
+      if (destination === clientId) {
+        // delete cookies addressed directly to this client,
+        // but not cookies which are broadcasted
+        document.cookie = key + "=;SameSite=None;Path=/;Secure;Max-Age=0";
+        console.log("deleting cookie", key);
+      }
+      observedKeys.add(key);
+      if (destination === clientId || destination === "ALL") {
+        if (!processedMessages.has(key)) {
+          processedMessages.add(key);
+          postToParent(data);
+          // const payload = JSON.parse(data)
+        }
+      }
+    }
+    for (let key of processedMessages) {
+      if (!observedKeys.has(key)) {
+        processedMessages.delete(key);
+        console.log("removing expired key from processed messages list", key);
+      }
+    }
+  };
+
+  const flushCookies = () => {
+    if (
+      pendingCookies.length > 0 &&
+      document.cookie.length + pendingCookies[0].length < 4096
+    ) {
+      document.cookie = pendingCookies.shift();
+    }
+    if (pendingCookies.length > 0) {
+      console.log("pending cookies wait for space");
+      setTimeout(flushCookies, 50);
+    }
+  };
+  let channel;
+  if (handle && handle.BroadcastChannel) {
+    channel = handle.BroadcastChannel("caddrRPC");
+    channel.onmessage = (e) => {
+      const payload = JSON.parse(e.data);
+      if (payload.to === "*" || payload.to === clientId) {
+        postToParent(e.data);
+      }
+    };
+  }
+  window.addEventListener(
+    "message",
+    function (event) {
+      const data = event.data;
+      const payload = JSON.stringify({
+        from: clientId,
+        fromOrigin: event.origin,
+        to: data.to,
+        method: data.method,
+        params: data.params,
+        id: data.id,
+        result: data.result,
+        error: data.error,
+      });
+      if (channel) {
+        channel.postMessage(payload);
+      } else {
+        const key =
+          "rpc_" +
+          (data.to === "*" ? "ALL" : data.to) +
+          "_" +
+          Math.random().toString(36).substring(2, 15);
+        processedMessages.add(key);
+
+        const newCookie =
+          key +
+          "=" +
+          encodeURIComponent(payload) +
+          ";SameSite=None;Path=/;Secure;Max-Age=5";
+        console.log("cookie size", document.cookie.length);
+        pendingCookies.push(newCookie);
+        if (pendingCookies.length === 1) flushCookies();
+      }
+    },
+    false
+  );
+
+  postToParent(
+    JSON.stringify({
+      from: "",
+      fromOrigin: "",
+      to: clientId,
+      method: "onConnect",
+      params: { clientId: clientId, hasBroadcastChannel: !!channel },
+    })
+  );
+
+  // cookies which were there before initialization should be excluded
+  const initialCookies = document.cookie
+    .split(";")
+    .map((k) => k.trim().split("=").map(decodeURIComponent));
+  for (let [key, data] of initialCookies) processedMessages.add(key);
+
+  if (window.cookieStore) {
+    cookieStore.onchange = (e) => {
+      if (e.changed.length > 0) getValues();
+    };
+  } else {
+    setInterval(getValues, 50);
+  }
+
+  if (button.parentNode) {
+    button.remove();
+  }
+}
+
+function retryMountHandle() {
+  makeStorageRequest(
+    (handle) => mountHandle(handle),
+    (err) => {
+      console.error(err);
+    }
+  );
+}
+
+function makeStorageRequest(successCallback, errorCallback) {
+  document.requestStorageAccess({ all: true }).then(
+    (handle) => {
+      document.cookie = "test=42;SameSite=None;Path=/;Secure"; // safari has some thing where if there are ever no cookies it stops working so we make this long lived
+      if (document.cookie === "") {
+        errorCallback("Test cookie not set");
+      } else {
+        successCallback(handle);
+      }
+    },
+    (err) => {
+      errorCallback(err);
+    }
+  );
+}
+
+// For Safari, we need to show a popup to get storage access. This
+// handles when caddr is loaded from a popup.
+// TODO: check that this is not loaded from a frame
+if (url.searchParams.get("authPopup") || url.searchParams.get("authRedirect")) {
+  button.innerText = "Grant storage access";
+  button.style.top = 0;
+  button.style.left = 0;
+  button.style.position = "absolute";
+  button.style.width = "100vw";
+  button.style.height = "100vh";
+  button.onclick = () => {
+    makeStorageRequest(
+      () => {
+        if (url.searchParams.get("authRedirect")) {
+          location.href = url.searchParams.get("authRedirect");
+        } else {
+          if (window.opener) window.opener.retryMountHandle();
+          window.close();
+        }
+      },
+      () => {}
+    );
+  };
+  document.body.appendChild(button);
+} else if (document.hasStorageAccess) {
+  const launchPopup = () => {
+    let popupUrl = new URL(location.href);
+    if (url.searchParams.get("useRedirect")) {
+      // popupUrl.searchParams.set("authRedirect", "redirect URL");
+      window.parent.postMessage(
+        { action: "redirect", value: popupUrl.toString() },
+        "*"
+      );
+    } else {
+      popupUrl.searchParams.set("authPopup", "true");
+      if (
+        !window.open(popupUrl, "_blank", "width=300,height=200,top=100,popup")
+      ) {
+        console.log("no opener available");
+      }
+    }
+  };
+  const showRequestButton = () => {
+    button.innerText = "Grant storage access";
+    button.style.top = 0;
+    button.style.left = 0;
+    button.style.position = "absolute";
+    button.style.width = "100vw";
+    button.style.height = "100vh";
+    button.onclick = () => {
+      makeStorageRequest(mountHandle, launchPopup);
+    };
+    document.body.appendChild(button);
+  };
+  navigator.permissions
+    .query({ name: "storage-access" })
+    .catch((err) => ({ state: "granted" }))
+    .then((res) => {
+      if (res.state === "granted") {
+        makeStorageRequest(mountHandle, showRequestButton);
+      } else if (res.state === "prompt") {
+        // current stable version of chrome may have storage access but state == prompt, but
+        // actually support requests without intervention
+        document.hasStorageAccess().then((hasAccess) => {
+          if (hasAccess) {
+            makeStorageRequest(mountHandle, showRequestButton);
+          } else {
+            showRequestButton();
+          }
+        });
+      }
+    });
+} else {
+  mountHandle(null);
+}
